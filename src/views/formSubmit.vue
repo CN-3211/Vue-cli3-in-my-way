1e36dda7eb18ff15d9e7bd7e914e640bdced491a
黄子涵 <372083746@qq.com>
2019-09-06 11:54:12 +0800

    form数据添加 table布局

--- src/views/formSubmit.vue
+++ src/views/formSubmit.vue
@@ -1,37 +1,73 @@
 <template>
   <div class="formSubmit">
-    <ul>
-      <li v-for="item in list" :key="item.id">
-        <p>{{item.article_title}}</p>
-        <p>{{item.article_header}}</p>
-      </li>
-    </ul>
+    <el-table border :data="tableData" stripe>
+      <el-table-column v-for="col in columns"
+        :prop="col.id"
+        :key="col.id"
+        :label="col.label"
+        :width="col.width">
+      </el-table-column>
+    </el-table>
+    
   </div>
 </template>
 <script>
 import { getFakeJson } from '~api';
+const columns = [
+  {
+    id: 'first',
+    label: 'id',
+    width: 200
+  },
+  // 这里的id就对应tableData的传入值
+  {
+    id: 'second',
+    label: 'title',
+    width: 200
+  }
+];
+
 export default {
   data () {
+    this.columns = columns;
     return {
-      list: [],
-      dataFake: [],
-      value: []
+      tableData: [],
     }
   },
   async mounted () {
-    let data = await getFakeJson()
+    let data = await getFakeJson({userId: 1});
     console.log('data :', data);
-  },
-  methods: {
-    // setFake(params) {
-    //   getFake(params).then( data => {
-    //     this.dataFake = data;
-    //     this.dataFake.forEach(item => {
-    //       this.list.push(Object.keys)  
-    //       console.log('this.list :', this.list);
-    //     })
-    //   })
-    // }
+    data.forEach(item => {
+      const { id, title } = item;
+      console.log('item :', id, title);
+      this.tableData.push({
+        first: id,
+        second: title
+      })
+    })
+    console.log('data :', data);
   }
 }
 </script>
+<style lang="scss" scoped>
+.formSubmit{
+  .el-table::before, .el-table::after{
+    background-color: #D8D8D8;
+  }
+  .el-table__header th {
+    background-color: #fafafa;
+    font-size: 16px;
+    border-color: #D8D8D8 !important;
+    .cell {
+      color:rgba(0,0,0,0.65);
+    }
+  }
+  .el-table__body-wrapper .cell {
+    color:rgba(0,0,0,0.65);
+    font-size: 14px;
+  }
+  .el-table__body td, .el-table--border{
+    border-color: #D8D8D8 !important;
+  }
+}
+</style>
