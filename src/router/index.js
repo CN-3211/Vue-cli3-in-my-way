9927941a462141376551d9ae440529927d315513
黄子涵 <372083746@qq.com>
2019-09-24 08:25:09 +0800

    新增测试单页

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,11 @@ export default new VueRouter({
       path: '/banner',
       name: 'banner',
       component: () => import(/* webpackChunkName: "about" */ '~views/banner')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import(/* webpackChunkName: "about" */ '~views/test')
     }
   ]
 })
---
+++ src/views/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap">
+    <div @click="params => handleClick(params, title)">两个参数</div>
+    <div @click="onParams">一个参数</div>
+    <div :title="title"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: '测试传入参数'
+    }
+  },
+  methods: {
+    handleClick(params, title){
+      console.log('params :', params);
+      console.log('title :', title);
+    },
+    onParams(params){
+      console.log('params :', params);
+    }
+  }
+}
+</script>
