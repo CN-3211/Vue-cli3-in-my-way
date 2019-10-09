463618b2f72c9fa6cf3f23d496195eee57065a8e
黄子涵 <372083746@qq.com>
2019-10-09 23:19:24 +0800

    解构赋值

--- docs/.vuepress/sideBar.js
+++ docs/.vuepress/sideBar.js
@@ -10,6 +10,7 @@ module.exports = [
     title: 'Javascript',
     children: [
       ['/JavaScript/JavaScript对象操作方法------删除一个键值对', 'Object删除键值对'],
+      ['/JavaScript/解构赋值多种用法', '解构赋值多种用法'],
     ]
   }
 ];
