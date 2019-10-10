ded2dc33497ebc756a1d3b2297a2489e71b5fde8
黄子涵 <372083746@qq.com>
2019-10-10 20:46:35 +0800

    更改目录结构 准备开始vue-leaflet文档编写

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,6 +1,7 @@
 module.exports = [
   { text: 'Home', link: '/' },
-  { text: 'Vue', link: '/vue/Object优化' },
+  { text: 'Blog', link: '/blog/vue/Object优化' },
+  { text: 'Vue-leaflet', link: '/vue-leaflet/test' },
   // { text: 'JavaScript', link: '/javaScript/' },
   { text: 'External', link: 'https://www.baidu.com' }
 ];
