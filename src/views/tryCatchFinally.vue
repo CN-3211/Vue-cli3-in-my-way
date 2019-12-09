d7e76e07b959d9a0041d75e39aacddb1648d09cb
黄子涵 <372083746@qq.com>
2019-12-09 16:58:50 +0800

    try-catch-finally

---
+++ src/views/tryCatchFinally.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: your name
+ * @Date: 2019-12-06 11:18:17
+ * @LastEditTime: 2019-12-09 13:40:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Vue-cli3-in-my-way\src\views\tryCatchFinally.vue
+ -->
+<template>
+  <div class="tryCatchFinally">
+    <p>{{errorMessage}}</p>
+    <p>{{message}}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      errorMessage: '',
+      message: ''
+    }
+  },
+  mounted() {
+    try {
+      alerdddt('1111')
+    } catch (error) {
+      this.errorMessage = error
+    } finally {
+      this.message = '无论是否出现错误都会执行的内容';
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tryCatchFinally {
+
+}
+</style>
