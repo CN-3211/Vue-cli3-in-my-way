a2aac61429ad2560b6efa20f100a37114954ce8f
CN-3211 <372083746@qq.com>
2020-03-11 20:43:43 +0800

    利用函数式组件 实现改写element-ui的dialog组件

---
+++ src/views/functionalDialog.vue
@@ -0,0 +1,11 @@
+<!--此组件可以完全代替el-dialog-->
+<script>
+  export default {
+    render(createElement, hack) {
+      console.log('hack',hack);
+      // hack.children[0].componentOptions.propsData.title = hack.props.title;
+      // 透传
+      return createElement('el-dialog', hack.data, hack.children)
+    }
+  }
+</script>
