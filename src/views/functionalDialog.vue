436c98fae9cabf3fea3b905ad99a363ac7293e09
黄子涵 <372083746@qq.com>
2020-03-11 21:14:40 +0800

    改了下 少了个functional

--- src/views/functionalDialog.vue
+++ src/views/functionalDialog.vue
@@ -1,10 +1,9 @@
 <!--此组件可以完全代替el-dialog-->
 <script>
   export default {
+    functional: true,
     render(createElement, hack) {
       console.log('hack',hack);
-      // hack.children[0].componentOptions.propsData.title = hack.props.title;
-      // 透传
       return createElement('el-dialog', hack.data, hack.children)
     }
   }
